733a25189392c9843381224381de1667cc51b70b
Daniel-Wmr <d.weimer777@gmail.com>
2026-02-03 17:54:03 +0000

    Final Solution Sequenz 1

--- courses/coding-in-C/Lab_2/my_solutions/chessboard_S1.c
+++ courses/coding-in-C/Lab_2/my_solutions/chessboard_S1.c
@@ -3,13 +3,14 @@
 int main(){
 
     char array_letters[9] = {'A','B','C','D','E','F','G','H'};
+    
     for(int i=8; i>0; i--){
         short number = i;
         for(int f=0; f<8; f++){
             char letter = array_letters[f];
             printf("%c%hd ", letter, number);
             if(f == 7){
-                printf("\n");
+                printf("\n\n");
             }
         }
     }
