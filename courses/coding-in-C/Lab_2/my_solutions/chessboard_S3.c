662727c88b698e0d064ad8b6b9253f4217583c6b
Daniel-Wmr <d.weimer777@gmail.com>
2026-02-03 23:17:09 +0000

    integrate array_numbers for better clarity

--- courses/coding-in-C/Lab_2/my_solutions/chessboard_S3.c
+++ courses/coding-in-C/Lab_2/my_solutions/chessboard_S3.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 char array_letters[9] = {'A','B','C','D','E','F','G','H'};
-char array_numbers[9] = {'1','2','3','4','5','6','7','8'};
+char array_numbers[9] = {1,2,3,4,5,6,7,8};
 
 int main(){
 
-    int counter_numbers = 8;
-    for(int r=0; r<19; r++){        
+    int counter_numbers = 0;
+    for(int r=0; r<=18; r++){        
         if(r==0 || r==18){
             printf(" ");
             for(int i=0; i<8; i++){
@@ -20,7 +20,7 @@ int main(){
             }
         }
         else {
-            printf("%d |", counter_numbers);
+            printf("%d |", array_numbers[counter_numbers]);
             int colour_code;
             if(counter_numbers%2 == 0){
                 colour_code = 0;
@@ -37,8 +37,8 @@ int main(){
                     printf("   |");
                 }
             }
-            printf(" %d", counter_numbers);
-            counter_numbers--; 
+            printf(" %d", array_numbers[counter_numbers]);
+            counter_numbers++; 
         }
     
         printf("\n");
