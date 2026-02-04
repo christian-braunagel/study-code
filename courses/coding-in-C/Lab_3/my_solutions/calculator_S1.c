dd6a3e6a29346ba36cd0a3624549053ea5f99850
Daniel-Wmr <d.weimer777@gmail.com>
2026-02-04 08:23:53 +0000

    prompt the two numbers

---
+++ courses/coding-in-C/Lab_3/my_solutions/calculator_S1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Type in the two numbers you want to offset:\n");
+    float first_number;
+    scanf("%f", &first_number);
+    float second_number;
+    scanf("%f", &second_number);
+    printf("Your two numbers: %f, %f\n\n", first_number, second_number);
+
+    
+
+    return 0;
+}
